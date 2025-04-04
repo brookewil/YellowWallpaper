4a237b3867634be51e4de78cf18f47269d50fb0b
Brooke Wilson <brookewil@comcast.net>
2025-04-04 13:40:15 -0400

    added jump blueprint

---
+++ Source/YellowWallpaper/YellowWallpaperPlayerHUD.h
@@ -0,0 +1,12 @@
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+UCLASS(Abstract)
+class YELLOWWALLPAPER_API UYellowPaperPlayerHUD : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+};
